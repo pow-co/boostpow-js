29ee7cd866984924c90f9f350c20e836e10d6a0c
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2022-07-26 08:44:29 -0500

    fix(Output): Output constructor did not correctly check for undefined values.

--- lib/output.ts
+++ lib/output.ts
@@ -12,14 +12,14 @@ export class Output {
   _txid: Digest32 | undefined
   _vout: number | undefined
 
-  constructor(job: Job, value?: number, txid?: Digest32, vout?: number) {
+  constructor(job: Job, value?: bigint, txid?: Digest32, vout?: number) {
     this.script = job
-    if (value) this._value = value
-    else if (!job.value) throw "invalid output: missing parameter value"
-    if (txid) this._txid = txid
-    else if (!job.txid) throw "invalid output: missing parameter txid"
-    if (vout) this._vout = vout
-    else if (!job.vout) throw "invalid output: missing parameter vout"
+    if (value !== undefined) this._value = value
+    else if (job.value === undefined) throw "invalid output: missing parameter value"
+    if (txid !== undefined) this._txid = txid
+    else if (job.txid === undefined) throw "invalid output: missing parameter txid"
+    if (vout !== undefined) this._vout = vout
+    else if (job.vout === undefined) throw "invalid output: missing parameter vout"
   }
 
   static fromTransaction(tx: bsv.Transaction | Buffer, vout: number): Output | undefined {
